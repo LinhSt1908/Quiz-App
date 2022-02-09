25a3c2ca5f454c25924a6a619b5f91f6ddb1d008
Thuy Linh Storck <thuylinh90@outlook.de>
2022-02-09 18:38:14 +0100

    letzte anpassung

--- style/components/Card.stories.js
+++ style/components/Card.stories.js
@@ -26,6 +26,24 @@ export const Default= () => /* html */ `
   <div class="boxSmall">A</div> <div class="boxSmall">B</div> <div class="boxSmall">C</div> <div class="boxSmall">D</div>
   </div>
   </div>
+
+  <div class="Box1">
+  <center><div class="Card__question"><strong>Frage 3</strong></div><center>
+  <p class="Card__content">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet</p>
+  <button class="Card__answer">Antwort</button>
+  <div class="Card__choice">
+  <div class="boxSmall">A</div> <div class="boxSmall">B</div> <div class="boxSmall">C</div> <div class="boxSmall">D</div>
+  </div>
+  </div>
+
+  <div class="Box1">
+  <center><div class="Card__question"><strong>Frage 4</strong></div><center>
+  <p class="Card__content">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet</p>
+  <button class="Card__answer">Antwort</button>
+  <div class="Card__choice">
+  <div class="boxSmall">A</div> <div class="boxSmall">B</div> <div class="boxSmall">C</div> <div class="boxSmall">D</div>
+  </div>
+  </div>
 `;
 
 export const Bookmarked= () => /* html */ `
@@ -38,6 +56,16 @@ export const Bookmarked= () => /* html */ `
 `;
 
 export const WithAnswer= () => /* html */ `
+  <article class="Card Card--error">
+    <h4 class="Card__title">Quiz App</h4>
+    <p class="Card__content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nemo
+      eum 
+    </p>
+  </article>
+`;
+
+export const Footer= () => /* html */ `
   <article class="Card Card--error">
     <h4 class="Card__title">Quiz App</h4>
     <p class="Card__content">
