d683f00f9b819afe0ba4048fab4957b6dd1b2ce5
Thuy Linh Storck <thuylinh90@outlook.de>
2022-02-24 18:26:34 +0100

    buttons finally work

--- button.js
+++ button.js
@@ -1,6 +1,11 @@
-const cardButton = document.querySelector('[data-js="answer-button"]');
-const cardAnswer = document.querySelector('[data-js="final-answer"]');
+const dropdown = document.querySelectorAll('[data-js="answer"]');
 
-cardButton.addEventListener('click', () => {
-  cardAnswer.classList.toggle('visible');
+dropdown.forEach(x => {
+  const finalAnswer = x.querySelector('[data-js="final-answer"]');
+  const answerButton = x.querySelector('[data-js="answer-button"]');
+
+  answerButton.addEventListener('click', () => {
+    finalAnswer.classList.toggle('visible');
+  });
 });
+
